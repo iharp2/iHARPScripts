677dc8cf216881241f98fa55d5ea1f1c2f4a4bf3
joHussien <jocode00@gmail.com>
2024-06-04 13:12:02 -0500

    Added getRaster fucntion, edited requirements acordingingly and added extractDateTimeInfo fucntion which will help in extracting some variables from the date and time but not now

--- iHARPExecutor.py
+++ iHARPExecutor.py
@@ -1,10 +1,34 @@
 import xarray as xr
-
+from datetime import datetime,timedelta
+import os
+import calendar
 
 class iHARPExecuter():
     def __init__(self):
-        pass
+        self.current_directory = '/data/iHARPVFullStack/iHARPV/api/'
+        self.dataDirectory = '/data/era5/'
+        self.variable, self.variable_unit,self.variable_long_name,self.time_resolution,self.time_agg_method,self.spatial_resolution,self.spatial_agg_method = "","","","","","",""
+        self.frames_directory = os.path.join(self.current_directory, 'assets/frames')
+        self.min_lat,self.max_lat,self.min_lon,self.max_lon = -90,90,-180,180
+    #This function is used in plotting
+    def extract_date_time_info(self,start_date_time_str, end_date_time_str):
+        # Parse start date-time string
+        start_date_time = datetime.fromisoformat(start_date_time_str.rstrip("Z"))
+        # Parse end date-time string
+        end_date_time = datetime.fromisoformat(end_date_time_str.rstrip("Z"))
 
+        # Extract information
+        self.selected_month_name_start = str(start_date_time.strftime("%B")).lower()
+        self.selected_month_name_end = str(end_date_time.strftime("%B")).lower()
+        self.selected_month_start = datetime.strptime(self.selected_month_name_start, '%B').month
+        self.selected_month_end = datetime.strptime(self.selected_month_name_end, '%B').month
+        self.selected_hour_start = start_date_time.strftime("%H")
+        self.selected_hour_end = end_date_time.strftime("%H")
+        self.selected_day_start = start_date_time.strftime("%d").lstrip("0")
+        self.selected_day_end = end_date_time.strftime("%d").lstrip("0")
+        self.selected_year_start = start_date_time.strftime("%Y")
+        self.selected_year_end = end_date_time.strftime("%Y")
+        return    
 
     def getRaster(
         self,
@@ -13,17 +37,71 @@ class iHARPExecuter():
         end_datetime: str,
         time_resolution: str,  # e.g., "hour", "day", "month", "year"
         time_agg_method: str,  # e.g., "mean", "max", "min"
-        min_lat: float,
-        max_lat: float,
-        min_lon: float,
-        max_lon: float,
+        south: float,
+        north: float,
+        west: float,
+        east: float,
         spatial_resolution: float,  # e.g., 0.25, 0.5, 1.0, 2.5, 5.0
         spatial_agg_method: str,  # e.g., "mean", "max", "min"
     ) -> xr.Dataset:
-        pass
+        
+        self.min_lat, self.max_lat, self.min_lon, self.max_lon,self.temporalLevel,self.time_agg_method,self.variable = float(south),float(north),float(west),float(east), time_resolution,time_agg_method,variable
+        self.spatial_resolution,self.spatial_agg_method = float(spatial_resolution),spatial_agg_method
+        if self.min_lon > self.max_lon:
+            self.max_lon, self.min_lon = self.min_lon, self.max_lon
+            self.max_lat, self.min_lat = self.min_lat, self.max_lat
+        self.lon_range,self.lat_range = slice(self.min_lon, self.max_lon),slice(self.max_lat, self.min_lat) 
+        self.startDateTime,self.endDateTime= start_datetime[:-11],end_datetime[:-11]
+        self.extract_date_time_info(self.startDateTime,self.endDateTime) 
+        start_date_counter = datetime.strptime(self.startDateTime, "%Y-%m-%dT%H")
+        end_date_counter = datetime.strptime(self.endDateTime, "%Y-%m-%dT%H")
+
+        if self.time_resolution=="Hourly":
+            #CASE #1: Requested hours in more than more than one year #This is EXPENSIVE CASE
+            if (str(self.selected_year_start) != str(self.selected_year_end)) :
+                ds_list = []
+                current_date = start_date_counter
+                while current_date <= end_date_counter:
+                    data_location = (self.dataDirectory+ 'raw/'+ self.variable+'/'+ self.variable+'-'+str(current_date.year)+'.nc')
+                    # For the first year, retrieve data from selected start datetime till the end of the year
+                    if current_date.year == self.selected_year_start:
+                        ds = xr.open_dataset(data_location, engine="netcdf4").sel(time=slice(self.startDateTime,None),longitude=self.lon_range, latitude=self.lat_range)
+                    # For the last year, retrieve data from selected start datetime till the end of the year
+                    elif current_date.year == self.selected_year_end:
+                        ds = xr.open_dataset(data_location, engine="netcdf4").sel(time=slice(None,self.endDateTime),longitude=self.lon_range, latitude=self.lat_range)
+                    # For all years between get all hours in thos years
+                    else:
+                        ds = xr.open_dataset(data_location, engine="netcdf4").sel(longitude=self.lon_range, latitude=self.lat_range)
+                        ds_list.append(ds)
+                    current_date += timedelta(years=1)
+                ds = xr.concat(ds_list, dim="time")
+
+            #CASE #2 Requested range of hours in one year
+            else:
+                data_location = (self.dataDirectory+'raw/'+  self.variable+'/'+ self.variable+'-'+str(self.selected_year_start)+'.nc')
+                ds = xr.open_dataset(data_location, engine="netcdf4").sel(time=slice(self.startDateTime,self.endDateTime),longitude=self.lon_range, latitude=self.lat_range)
+                
+        else:
+            print(self.dataDirectory+'preprocessed/' +self.variable+'/'+ 'combined_'+str(self.time_resolution).lower() + str(self.time_agg_method)+"_2014_2023.nc")
+            data_location = self.dataDirectory+'preprocessed/' +self.variable+'/'+ 'combined_'+str(self.time_resolution).lower() + str(self.time_agg_method)+"_2014_2023.nc"
+            ds = xr.open_dataset(data_location).sel(time=slice(self.startDateTime,self.endDateTime),longitude=self.lon_range, latitude=self.lat_range)
+        
+        if self.spatial_resolution!=0.25:
+            coarsened = ds.coarsen(latitude=self.spatial_resolution/0.25,longitude=self.spatial_resolution/0.25)
+            if self.spatial_agg_method=='mean':
+                ds = coarsened.mean()
+            elif self.spatial_agg_method=='min':
+                ds = coarsened.min()
+            elif self.spatial_agg_method=='max':
+                ds = coarsened.max()    
+        self.raster = ds
+
+        return self.raster
+        
 
 
     def viewTimeSeries(self, raster: xr.Dataset):
+
         pass
 
 
